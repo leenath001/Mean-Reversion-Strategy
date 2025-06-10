c5229c154b343f8a5e4060f589b17c1dd28122a3
Nathaniel Lee <leenathaniel001@gmail.com>
2025-06-10 15:23:37 -0400

    Update SMA_execution.py

--- SMA_execution.py
+++ SMA_execution.py
@@ -1,18 +1,8 @@
-import SMA_functions as SMA_functions
-import yfinance as yf
+from MRclass import MeanReversion
 
-# terminal func to run while in clamshell, ensure amphetaime is on
+# add: 
+# - stop loss (if z goes up in 2 ticks, close, z does down in 2 ticks close) 
+# - regime identification
 
-# macair
-# caffeinate -i python3 "{filepath}"
-
-# macmini
-# caffeinate -i python3 ""{filepath}""
-
-#x = SMA.SMA_backtest('TSLA',4,2025,'ov')
-# (TSLA,9)
-
-x = SMA_functions.SMA_tradingfunc('SPY',4,'mr')
-print()
-print(x[0]) # dataframe contains time-series data containing algorithmic actions, buy/hold quotes, and strategy values
-print(x[1]) # trading stats  (alpha and beta are asset-relative, not marked to overall market)
+mr = MeanReversion(ticker="SPY", window=30, alo=50)
+mr.run() 
